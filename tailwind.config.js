74cc2ef2b29baf4b9d2fbe5fc11566bf9db73caf
beringui1611 <fortiniteberingui12@gmail.com>
2024-04-12 16:13:11 -0300

    new

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'uber': "url('/src/uber/assets/bguber.png')",
+        'city': "url('/src/uber/assets/bg-city.png')",
+        "nike": "url(/src/nike/assets/bg.png)"
+    },
+      keyframes: {
+        floatUp: {
+          "0%": { transform: "translatey(0px)" },
+          "50%": { transform: "translatey(-8px)" },
+          "100%": {transform: "translatey(0px)"}
+        }
+      },
+      animation: {
+        floatUp: "floatUp 2s infinite"
+      },
+      colors: {
+        primaria: "#ffff"
+      },
+      fontFamily: {
+        "sans": ["Montserrat"]
+      }
+    },
   },
   plugins: [],
 }
